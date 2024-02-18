6e2d251c2770462b2cb204551d348a60b752c3a6
akshayru7 <160200838+akshayru7@users.noreply.github.com>
2024-02-18 07:04:23 +0530

    Update MultidimentionalArray.java
    some changes in adding values

--- src/test/java/JavaA7Test/MultidimentionalArray.java
+++ src/test/java/JavaA7Test/MultidimentionalArray.java
@@ -7,7 +7,7 @@ public class MultidimentionalArray {
 
 		int rollno [][] = { {1,2,3,4,5}, {15,16,17,18,19}, {6,7,8,9,10}, {22,23,24,25,26}, {50,51,55,54,55} };
 		
-		rollno[0][2]=7;
+		rollno[0][3]=7;
 		
 		System.out.println("Roll No : " + rollno[4][4]);
 		System.out.println("Rows Length : " + rollno.length);
